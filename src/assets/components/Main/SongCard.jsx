85ab7f783a32610586c6ca965953a1a5f838dcee
D'Shon Taylor <dshontaylor@gmail.com>
2025-06-04 22:25:54 -0500

    styling of remove button

--- src/assets/components/Main/SongCard.jsx
+++ src/assets/components/Main/SongCard.jsx
@@ -4,20 +4,30 @@ import playlistStyles from './Playlist.module.css';
 
 function SongCard({title, artist, album, cover, addedToPlaylist, playlistCard, onAdd, onRemove}) {
 
-    const buttonClass = playlistCard ? 'remove-from-playlist' : 'add-to-playlist';
+    const [removing, setRemoving] = useState(false);
+
+    const buttonClass = playlistCard ? `remove-from-playlist ${playlistCard && removing ? ' remove-animation' : ''}` : 'add-to-playlist';
 
     const handleClick = () => {
+        if (removing) return;
+
         if (!playlistCard && !addedToPlaylist) {
             onAdd();
-        } else if (addedToPlaylist) {
+        } else if (!playlistCard && addedToPlaylist) {
             onRemove();
+        } else if (addedToPlaylist) {
+            setRemoving(true);
+            setTimeout( () => {
+                onRemove()
+            }, 500);
         }
     };
 
     const cardClassName = [
         styles.songCard,
         playlistCard ? playlistStyles.playlistCard : '',
-        !playlistCard && addedToPlaylist ? 'selected' : ''
+        !playlistCard && addedToPlaylist ? 'selected' : '',
+        playlistCard && removing ? 'remove-entry-animation' : ''
     ].join(' ');
 
     return (
@@ -25,9 +35,13 @@ function SongCard({title, artist, album, cover, addedToPlaylist, playlistCard, o
             <img src={cover} title={`Album art for ${title} by ${artist}`} />
             <div className={styles.songInfo}>
                 <h3>{title}</h3>
-                <p>{artist} { addedToPlaylist ? '' : <span className={styles.albumTitle}>| {album}</span>}</p>
+                <p>{artist} { !playlistCard ? <span className={styles.albumTitle}>| {album}</span>: ''}</p>
             </div>
-            <span className={buttonClass} onClick={handleClick}></span>
+            <span
+                className={buttonClass}
+                onClick={handleClick}
+                style={removing ? { pointerEvents: 'none' } : {}}
+            />
         </div>
     );
 }
